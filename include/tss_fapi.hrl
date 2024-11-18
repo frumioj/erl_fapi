fc0dd58f0e8b7ee189af75122e8e0345091ac8c3
John Kemp <stable.pseudonym@gmail.com>
2024-11-18 07:06:36 -0500

    adding definitions

---
+++ include/tss_fapi.hrl
@@ -0,0 +1,9 @@
+-define(TSS_KEY_TYPE_RSA, rsa).
+-define(TSS_KEY_TYPE_ECC, ecc).
+
+-define(TSS_SIGN_SCHEME_RSA_PSS, rsa_pss).
+-define(TSS_SIGN_SCHEME_RSA_SSA, rsa_ssa).
+-define(TSS_SIGN_SCHEME_ECDSA, ecdsa).
+
+-define(TSS_ENCRYPT_SCHEME_RSA_OAEP, rsa_oaep).
+-define(TSS_ENCRYPT_SCHEME_RSA_PKCS1, rsa_pkcs1).
